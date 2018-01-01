b63145142c5f360cca50be3cdbe35daf362cbf10
91910lin <34039598+91910lin@users.noreply.github.com>
2018-01-02 00:20:10 +0800

    V2

---
+++ app/src/main/java/com/example/a91910/myapplication/MainActivity.java
@@ -0,0 +1,204 @@
+package com.example.a91910.myapplication;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Configuration;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class MainActivity extends Activity {
+	private final String PREFS_TASKS = "prefs_tasks";
+	private final String KEY_TASKS_LIST = "list";
+	private final int ADD_TASK_REQUEST = 1;
+	private static final String TAG = MainActivity.class.getName();
+	private BroadcastReceiver mTickReceiver;
+	private ArrayList<String> mList;
+	private ArrayAdapter<String> mAdapter;
+	private TextView mDateTimeTextView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// 1
+		super.onCreate(savedInstanceState);
+
+		// 2 -Make the activity full screen
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		// 3
+		setContentView(R.layout.activity_main);
+
+
+		TabHost host = (TabHost)findViewById(R.id.tabHost);
+		host.setup();
+
+		TabHost.TabSpec spec;
+
+		spec=host.newTabSpec("Tab1");
+		spec.setIndicator("待完成事項");
+		spec.setContent(R.id.tab1);
+		host.addTab(spec);
+
+
+		spec=host.newTabSpec("Tab2");
+		spec.setIndicator("已完成事項");
+		spec.setContent(R.id.tab2);
+		host.addTab(spec);
+
+
+		// 4
+		mDateTimeTextView = (TextView) findViewById(R.id.dateTimeTextView);
+		final Button addTaskBtn = (Button) findViewById(R.id.addTaskBtn);
+		final ListView listview = (ListView) findViewById(R.id.taskListview);
+		mList = new ArrayList<String>();
+
+		String savedList = getSharedPreferences(PREFS_TASKS, MODE_PRIVATE).getString(KEY_TASKS_LIST, null);
+		if (savedList != null) {
+			String[] items = savedList.split(",");
+			mList = new ArrayList<String>(Arrays.asList(items));
+		}
+
+		// 5
+		mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mList);
+		listview.setAdapter(mAdapter);
+
+		// 6
+		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+				taskSelected(i);
+			}
+		});
+		mTickReceiver = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				if (intent.getAction().equals(Intent.ACTION_TIME_TICK)) {
+					mDateTimeTextView.setText(getCurrentTimeStamp());
+				}
+			}
+		};
+
+
+
+
+
+
+	}
+
+	@Override
+	protected void onResume() {
+// 1
+		super.onResume();
+// 2
+		mDateTimeTextView.setText(getCurrentTimeStamp());
+// 3
+		registerReceiver(mTickReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+	}
+
+	@Override
+	protected void onPause() {
+// 4
+		super.onPause();
+// 5
+		if (mTickReceiver != null) {
+			try {
+				unregisterReceiver(mTickReceiver);
+			} catch (IllegalArgumentException e) {
+				Log.e(TAG, "Timetick Receiver not registered", e);
+			}
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+
+// 保存數據
+		StringBuilder savedList = new StringBuilder();
+		for (String s : mList) {
+			savedList.append(s);
+			savedList.append(",");
+		}
+		getSharedPreferences(PREFS_TASKS, MODE_PRIVATE).edit()
+				.putString(KEY_TASKS_LIST, savedList.toString()).commit();
+	}
+
+	public void addTaskClicked(View view) {
+		Intent intent = new Intent(MainActivity.this, TaskDescriptionActivity.class);
+		startActivityForResult(intent, ADD_TASK_REQUEST);
+	}
+
+	private static String getCurrentTimeStamp() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");//dd/MM/yyyy
+		Date now = new Date();
+		String strDate = sdf.format(now);
+		return strDate;
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+// 1 - 判斷你需要處理的請求是否正確
+		if (requestCode == ADD_TASK_REQUEST) {
+// 2 - 判斷請求是否成功返回
+			if (resultCode == RESULT_OK) {
+// 3 - 用戶輸入了有效的待辦。將任務添加到任務列表。
+				String task = data.getStringExtra(TaskDescriptionActivity.EXTRA_TASK_DESCRIPTION);
+				mList.add(task);
+// 4
+				mAdapter.notifyDataSetChanged();
+			}
+		}
+	}
+
+	private void taskSelected(final int position) {
+// 1
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+
+// 2
+		alertDialogBuilder.setTitle("事項");
+
+// 3
+		alertDialogBuilder
+				.setMessage(mList.get(position))
+				.setPositiveButton("刪除", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						mList.remove(position);
+						mAdapter.notifyDataSetChanged();
+					}
+				})
+				.setNegativeButton("完成", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+
+// 4
+		AlertDialog alertDialog = alertDialogBuilder.create();
+
+// 5
+		alertDialog.show();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+	}
+}
